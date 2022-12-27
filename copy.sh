28a3a0cf0549ff19f74a43b1a1a59ae829f802bf
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2022-12-27 12:21:57 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -2,12 +2,22 @@
 # make sure that you can login without password by placing ssh keys.
 
 #!/bin/bash
-A='/Users/abc/Zr'
+A='/Users/abc/Hf'
 
-for (( i=1; i<=18; i++ ))
-do 
-	B=`head -$i $A/dir | tail -1`
-	C=`head -$i $A/dir | tail -1 | awk -F '/' '{print $1}'`
-	scp username@hostname:/lfs/usrhome/Zr/$B/POSCAR /Users/abc/Zr/$C/
-	scp username@hostname:/lfs/usrhome/Zr/$B/CONTCAR /Users/abc/Zr/$C/
+for (( i=1; i<=13; i++ ))
+do
+    B=`head -$i $A/dir | tail -1`
+    mkdir $B
+    scp user@hostname:/lfs/usrhome/Hf/$B/POSCAR $A/$B/
+    scp user@hostname:/lfs/usrhome/Hf/$B/CONTCAR $A/$B/
+done
+
+
+for (( i=14; i<=17; i++ ))
+do
+    B=`head -$i $A/dir | tail -1`
+    C=`head -$i $A/dir | tail -1 | awk -F '/' '{print $1}'`
+    mkdir $C
+    scp user@hostname:/lfs/usrhome/Hf/$B/POSCAR $A/$C/
+    scp user@hostname:/lfs/usrhome/Hf/$B/CONTCAR $A/$C/
 done
