e5c675bbd9c2d9b24e20109cfd94791d6c74008d
Prince Gollapalli <58852018+princego@users.noreply.github.com>
2022-12-27 11:09:29 +0530

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-A='/Users/prince/edu/IITM/1chemGrad/2Zr'
+# this scripts downloads POSCAR and CONTCAR files from a remote server to your computer.
+# make sure that you can login without password by placing ssh keys.
 
-for (( i=15; i<=18; i++ ))
+#!/bin/bash
+A='/Users/abc/Zr'
+
+for (( i=1; i<=18; i++ ))
 do 
 	B=`head -$i $A/dir | tail -1`
 	C=`head -$i $A/dir | tail -1 | awk -F '/' '{print $1}'`
-	scp mm17d012@aqua.iitm.ac.in:/lfs/usrhome/phd/mm17d012/1chemGrad/Zr/$B/POSCAR /Users/prince/edu/IITM/1chemGrad/2Zr/$C/
-	scp mm17d012@aqua.iitm.ac.in:/lfs/usrhome/phd/mm17d012/1chemGrad/Zr/$B/CONTCAR /Users/prince/edu/IITM/1chemGrad/2Zr/$C/
+	scp username@hostname:/lfs/usrhome/Zr/$B/POSCAR /Users/abc/Zr/$C/
+	scp username@hostname:/lfs/usrhome/Zr/$B/CONTCAR /Users/abc/Zr/$C/
 done
